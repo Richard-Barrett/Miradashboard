c67a72080e55822f9e72042c21a86a9fe61ffcf3
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2019-01-18 15:43:47 -0600

    Create broke_sla_pick_severity_level.R

---
+++ broke_sla_pick_severity_level.R
@@ -0,0 +1,11 @@
+# Created by Richard Barrett
+# Date 01/19/2019
+# Version 1.0
+# ==========================
+
+# This report shows the cases with Broken SLA's by severity.
+# Shows that we are failing to close cases by SLA.
+# Quantify the frequency of cases with broken SLA's over time.
+# Aggregate of Cases over time
+# Quarterly and Bi-Weekly Metrics
+# Identify Cases that Brake SLA
