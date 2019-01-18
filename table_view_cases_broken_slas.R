d4e97e65dd47a216bce255d8d9aa3822a119817c
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2019-01-18 16:00:39 -0600

    Create table_view_cases_broken_slas.R

---
+++ table_view_cases_broken_slas.R
@@ -0,0 +1,9 @@
+# Created by Richard Barrett
+# Date: 01/19/2019
+# Version 1.0
+# ==========================
+
+# Tabular View for all cases with broken SLA's
+# Pulls Metrics off cses wtih broken SLA's
+# Generates a subset on the original pool of cases
+# Generates a view of broken cases in table form by Case Number, Severity Level, Case Owner, Status, Subject, Last Modified, Date Opened, 
