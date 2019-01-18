4c5ba7902ef04e3b8ae696a5eca0baa2ebbb3c08
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2019-01-18 16:17:30 -0600

    Update salesforce_change_requests.R

--- salesforce_change_requests.R
+++ salesforce_change_requests.R
@@ -1,1 +1,18 @@
+# Created by Richard Barrett 
+# Date 01/18/2019
+# Version 1.0
+# ===========================
+# Note: report to be given every other week bi-weekly
+# Change Requests that don't have attached MW Links
 
+# Tabular View for MW with MW Numbers
+# =====================================================================================
+# Check for Change Requests by Case Number, Case Owner, Status
+# Check to see who is performing CR on Clouds
+# Change Requests Performed by Services, DE, or Support that wasn't logged
+# Change Requests from each group Services & Support
+# Quanitfy Commits mapped to Change Requests (Every Chage Request has to have a commit)
+# Look at frequency of changes over time for two week time periods.
+# Future Map a commit to a change request
+# Quantify who proposed change, was procedure written?
+# Find cases that experienced change without owners for CRs and Commits
