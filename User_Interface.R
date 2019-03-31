15127f182e894b40b0301fd62ef255a38a709194
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2019-03-30 19:36:56 -0500

    Update User_Interface.R

--- User_Interface.R
+++ User_Interface.R
@@ -11,110 +11,112 @@
 library(shinydashboardPlus)
 library(shinydashboard)
 library(shiny)
+library(rsconnect)
+library(googlesheets)
 
 #shinyApp(ui = ui, server = server, options = list(height = 1080))
 
-# User Interface Body Code is foun below:
-# =======================================
 ui <- dashboardPage(skin = "red",
-  dashboardHeader(title = "Miradashboard",
-                  # This drop-down menu offers user and system administration within the application
-                  dropdownMenu(type = "messages",
-                               messageItem(
-                                 from = "Sales Dept",
-                                 message = "Sales are steady this month."
-                               ),
-                               messageItem(
-                                 from = "New User",
-                                 message = "How do I register?",
-                                 icon = icon("question"),
-                                 time = "13:45"
-                               ),
-                               messageItem(
-                                 from = "Support",
-                                 message = "The new server is ready.",
-                                 icon = icon("life-ring"),
-                                 time = "2014-12-01"
-                               )
-                  ),
-                  # This is a drop-down menu for checking notifications.
-                  # This should alert users of alerts that have not been merged to a case in the last 15 days.
-                  dropdownMenu(type = "notifications",
-                               notificationItem(
-                                 text = "5 new users today",
-                                 icon("users")
-                               ),
-                               notificationItem(
-                                 text = "12 items delivered",
-                                 icon("truck"),
-                                 status = "success"
-                               ),
-                               notificationItem(
-                                 text = "Server load at 86%",
-                                 icon = icon("exclamation-triangle"),
-                                 status = "warning"
-                               )
-                  ),
-                  # This is a drop-down menu for checking tasks.
-                  # This drop-down menu will eventually offer suggestions based off of ML Algorithms.
-                  dropdownMenu(type = "tasks", badgeStatus = "success",
-                               taskItem(value = 90, color = "green",
-                                        "Documentation"
-                               ),
-                               taskItem(value = 17, color = "aqua",
-                                        "Project X"
-                               ),
-                               taskItem(value = 75, color = "yellow",
-                                        "Server deployment"
-                               ),
-                               taskItem(value = 80, color = "red",
-                                        "Overall project"
-                               )
-                  )
-                  
-                  
-                  
-  ),
-  dashboardSidebar(
-    ## Sidebar content
-    dashboardSidebar(
-      sidebarMenu(
-        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-        menuItem("Widgets", tabName = "widgets", icon = icon("th")),
-        menuItem("Reports", tabName = "reports", icon = icon("th")),
-        menuItem("OpsCare Clients", tabName = "OpsCare Clients", icon = icon("bar-chart-o")),
-        menuItem("ProdCare Clients", tabName = "ProdCare Clients", icon = icon("bar-chart-o")),
-        menuItem("Alerts", tabName = "Alerts", icon = icon("bar-chart-o")),
-        menuItem("Change Requests", tabName = "Change Requests", icon = icon("list-alt")),
-        menuItem("Maintenance Windows", tabName = "Maintenance Windows", icon = icon("list-alt")),
-        menuItem("Rundeck", tabName = "Rundeck", icon = icon("bars")),
-        menuItem("Salesforce", tabName = "Salesforce", icon = icon("bars")),
-        menuItem("Handovers", tabName = "Handovers", icon = icon("bars")),
-        menuItem("Jump-Host Access", tabName = "Jump-Host Access", icon = icon("bars"))
-      )
-    )
-  ),
-  dashboardBody(
-    # Boxes need to be put in a row (or column)
-    fluidRow(
-      box(plotOutput("plot1", height = 250)),
-      box(plotOutput("plot2", height = 250)),
-      box(plotOutput("plot3", height = 250)),
-      #box(plotOutput("plot4", height = 250)),
-      #box(dataTableOutput("DT1", height = 250)),
-      box(
-        title = "Controls",
-        sliderInput("slider", "Number of observations:", 1, 100, 50)
-      )
-    )
-  )
+                    dashboardHeader(title = "Miradashboard",
+                                    # This drop-down menu offers user and system administration within the application
+                                    dropdownMenu(type = "messages",
+                                                 messageItem(
+                                                   from = "Sales Dept",
+                                                   message = "Sales are steady this month."
+                                                 ),
+                                                 messageItem(
+                                                   from = "New User",
+                                                   message = "How do I register?",
+                                                   icon = icon("question"),
+                                                   time = "13:45"
+                                                 ),
+                                                 messageItem(
+                                                   from = "Support",
+                                                   message = "The new server is ready.",
+                                                   icon = icon("life-ring"),
+                                                   time = "2014-12-01"
+                                                 )
+                                    ),
+                                    # This is a drop-down menu for checking notifications.
+                                    # This should alert users of alerts that have not been merged to a case in the last 15 days.
+                                    dropdownMenu(type = "notifications",
+                                                 notificationItem(
+                                                   text = "5 new users today",
+                                                   icon("users")
+                                                 ),
+                                                 notificationItem(
+                                                   text = "12 items delivered",
+                                                   icon("truck"),
+                                                   status = "success"
+                                                 ),
+                                                 notificationItem(
+                                                   text = "Server load at 86%",
+                                                   icon = icon("exclamation-triangle"),
+                                                   status = "warning"
+                                                 )
+                                    ),
+                                    # This is a drop-down menu for checking tasks.
+                                    # This drop-down menu will eventually offer suggestions based off of ML Algorithms.
+                                    dropdownMenu(type = "tasks", badgeStatus = "success",
+                                                 taskItem(value = 90, color = "green",
+                                                          "Documentation"
+                                                 ),
+                                                 taskItem(value = 17, color = "aqua",
+                                                          "Project X"
+                                                 ),
+                                                 taskItem(value = 75, color = "yellow",
+                                                          "Server deployment"
+                                                 ),
+                                                 taskItem(value = 80, color = "red",
+                                                          "Overall project"
+                                                 )
+                                    )
+                                    
+                                    
+                                    
+                    ),
+                    dashboardSidebar(
+                      ## Sidebar content
+                      dashboardSidebar(
+                        sidebarMenu(
+                          menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+                          menuItem("Widgets", tabName = "widgets", icon = icon("th")),
+                          menuItem("Reports", tabName = "reports", icon = icon("chart-line")),
+                          menuItem("OpsCare Clients", tabName = "OpsCare Clients", icon = icon("bar-chart-o")),
+                          menuItem("ProdCare Clients", tabName = "ProdCare Clients", icon = icon("bar-chart-o")),
+                          menuItem("Alerts", tabName = "Alerts", icon = icon("bar-chart-o")),
+                          menuItem("Change Requests", tabName = "Change Requests", icon = icon("list-alt")),
+                          menuItem("Maintenance Windows", tabName = "Maintenance Windows", icon = icon("list-alt")),
+                          menuItem("Rundeck", icon = icon("code"), 
+                                   href = "https://rundeck.suplab01.snv.mirantis.net/user/login"),
+                          menuItem("Salesforce", icon = icon("database"), 
+                                   href = "https://mirantis.my.salesforce.com/"),
+                          menuItem("Handovers", icon = icon("google"), 
+                                   href = "https://docs.google.com/spreadsheets/d/1Wu8gJyzw6o7BS4GoR7pM_NofHyXvOzDMK3O-VVHcB8c/edit#gid=0"),
+                          menuItem("Jump-Host Access", tabName = "Jump-Host Access", icon = icon("bars")),
+                          menuItem("Mirantis HT Wiki", icon = icon("bars"), 
+                                   href = "https://mirantis.jira.com/wiki/spaces/2S/pages/1254621239/L1+-+General+Queue+Help+Desk+Team"),
+                          menuItem("Source code", icon = icon("github"), 
+                                   href = "https://github.com/Richard-Barrett/Miradashboard")
+                        )
+                      )
+                    ),
+                    dashboardBody(
+                      # Boxes need to be put in a row (or column)
+                      fluidRow(
+                        box(plotOutput("plot1", height = 250)),
+                        box(plotOutput("plot2", height = 250)),
+                        box(plotOutput("plot3", height = 250)),
+                        box(plotOutput("plot4", height = 250)),
+                        #box(dataTableOutput("DT1", height = 250))
+                        box(
+                          title = "Controls",
+                          sliderInput("slider", "Number of observations:", 1, 100, 50)
+                        )
+                      )
+                    )
 )
 
-
-
-
-# Output Blocks for Block Panes can e found below:
-# ================================================
 server <- function(input, output) {
   set.seed(122)
   histdata <- rnorm(500)
