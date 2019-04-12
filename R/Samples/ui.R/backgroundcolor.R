3604d168b05d8dc8e2a09243f7e95ab17b674f08
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2019-04-12 12:37:41 -0500

    Create backgroundcolor.R

---
+++ R/Samples/ui.R/backgroundcolor.R
@@ -0,0 +1,100 @@
+## Not run: 
+
+if (interactive()) {
+
+### Uniform color background :
+
+library(shiny)
+library(shinyWidgets)
+
+ui <- fluidPage(
+  tags$h2("Change shiny app background"),
+  setBackgroundColor("ghostwhite")
+)
+
+server <- function(input, output, session) {
+
+}
+
+shinyApp(ui, server)
+
+
+### linear gradient background :
+
+library(shiny)
+library(shinyWidgets)
+
+ui <- fluidPage(
+
+  # use a gradient in background
+  setBackgroundColor(
+    color = c("#F7FBFF", "#2171B5"),
+    gradient = "linear",
+    direction = "bottom"
+  ),
+
+  titlePanel("Hello Shiny!"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("obs",
+                  "Number of observations:",
+                  min = 0,
+                  max = 1000,
+                  value = 500)
+    ),
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  output$distPlot <- renderPlot({
+    hist(rnorm(input$obs))
+  })
+}
+
+shinyApp(ui, server)
+
+
+### radial gradient background :
+
+library(shiny)
+library(shinyWidgets)
+
+ui <- fluidPage(
+
+  # use a gradient in background
+  setBackgroundColor(
+    color = c("#F7FBFF", "#2171B5"),
+    gradient = "radial",
+    direction = c("top", "left")
+  ),
+
+  titlePanel("Hello Shiny!"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("obs",
+                  "Number of observations:",
+                  min = 0,
+                  max = 1000,
+                  value = 500)
+    ),
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  output$distPlot <- renderPlot({
+    hist(rnorm(input$obs))
+  })
+}
+
+shinyApp(ui, server)
+
+}
+
+
+## End(Not run)
