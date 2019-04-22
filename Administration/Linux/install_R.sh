87f6d561c709777be5b1a611edb856eda99a488e
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2019-04-22 12:46:44 -0500

    Update install_R.sh
    Added in the contents to install R Shiny Server under Administration

--- Administration/Linux/install_R.sh
+++ Administration/Linux/install_R.sh
@@ -1,2 +1,23 @@
 #!/bin/bash -x 
+# Created by: Richard Barrett
+# Creation Date: 04/22/2019
+# Purpose: Install R Shiny Server
+# Company: Mirantis 
+# ---------------------------------
+
+# The purpose of this script is to install and configure R Shiny Server
+
+# Script Will install R Shiny Base and Gdebi-Core
+sudo apt-get -f install r-base -y
+sudo apt-get -f install gdebi-core -y
+
+# Install Shiny Package
+sudo su - \ -c "R -e \"install.packages('shiny', repos='https://cran.rstudio.com/')\""
+
+# Download R Shiny 
+wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.9.923-amd64.deb
+sudo gdebi shiny-server-1.5.9.923-amd64.deb
+
+# --- END-SCRIPT ---
+
 
