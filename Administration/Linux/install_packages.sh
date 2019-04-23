6722e8d83c0f27ecb4aadd7dd569470c74369e9a
Richard Barrett <32808365+Richard-Barrett@users.noreply.github.com>
2019-04-22 21:51:53 -0500

    Create install_packages.sh

---
+++ Administration/Linux/install_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x 
+# Created by: Richard Barrett
+# Creation Date: 04/22/2019
+# Purpose: Install R Shiny Server
+# Company: Mirantis 
+# ---------------------------------
+
+# Script will install all of the required packages for the server
